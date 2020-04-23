6fb1f2480f9168cffff4c3239747ced0da0a9f8e
Isaac <isaac.io.ortega@gmail.com>
2020-04-23 13:23:13 -0700

    Remove light green as a potential background color.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Make background a random knotris color
-let knotrisColors = ["#31EBB6", "#CEFFB8", "#9CBFFB", "#FFAB62", "#EAED82"];
+let knotrisColors = ["#31EBB6", "#9CBFFB", "#FFAB62", "#EAED82"]; // No light-green cause it's difficult to read
 let randomIndex = Math.floor(Math.random() * Math.floor(knotrisColors.length));
 document.getElementsByTagName("BODY")[0].style.backgroundColor =
   knotrisColors[randomIndex];
