afc80bd817e7ad8dea00182ad7f2b24f36492af0
Isaac Ortega <isaac.io.ortega@gmail.com>
2020-10-12 22:10:44 -0700

    Update file structure

--- docs/scripts.js
+++ docs/scripts.js
@@ -10,25 +10,26 @@ function setUpGoogleAnalytics() {
 function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+  if (parts.length === 2) return parts.pop().split(";").shift();
 }
 
 // Opt out of google analytics on all my pages
 function optOut() {
-  window['ga-disable-UA-162642839-1'] = true;
-  document.getElementsByClassName("cookieBanner")[0].style.transform = "translateY(0%)";
+  window["ga-disable-UA-162642839-1"] = true;
+  document.getElementsByClassName("cookieBanner")[0].style.transform =
+    "translateY(0%)";
   document.cookie = "IZOOK_OPT_OUT=true";
   setTimeout(function hideBanner() {
-    document.getElementsByClassName("cookieBanner")[0].style.transform = "translateY(100%)"
-  }, 8000); 
+    document.getElementsByClassName("cookieBanner")[0].style.transform =
+      "translateY(100%)";
+  }, 8000);
 }
 
 // Check if user isn't opted out
-const optOutCookie = getCookie('IZOOK_OPT_OUT');
+const optOutCookie = getCookie("IZOOK_OPT_OUT");
 if (optOutCookie != "true") {
   setUpGoogleAnalytics();
 }
-
 
 // Make background a random knotris color
 let knotrisColors = ["#31EBB6", "#9CBFFB", "#FFAB62", "#EAED82"]; // No light-green cause it's difficult to read
